56133144d0a4060077d657d060e7d6a71abc2293
endris <endris@endris-PC>
2016-10-25 20:38:37 +0300

    creating spring-boot app; creating controller

--- src/main/java/com/example/homework/Application.java
+++ src/main/java/com/example/homework/Application.java
@@ -1,13 +1,16 @@
 package com.example.homework;
 
-//@SpringBootApplication
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        SpringApplication.run(Application.class, args);
     }
     
 }
---
+++ src/main/java/com/example/homework/FamousPeopleController.java
@@ -0,0 +1,19 @@
+package com.example.homework;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FamousPeopleController {
+    
+    private final AtomicLong counter = new AtomicLong();
+
+    
+    @RequestMapping(value = "/url-to-be-scanned", method = RequestMethod.POST)
+    public void urlToBeScanned(@RequestBody String urlToBeScanned) {
+        System.out.println("url to be scanned: "+urlToBeScanned);
+    }
+}
