672ec76916e3bec4ab3dd352a91512cf2849101a
endris <endris@endris-PC>
2016-10-25 20:14:39 +0300

    initial commit

---
+++ src/main/java/com/example/homework/Application.java
@@ -0,0 +1,13 @@
+package com.example.homework;
+
+//@SpringBootApplication
+public class Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
