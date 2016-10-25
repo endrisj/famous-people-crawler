1cf605e79412defe25c59a6ded2c4a697eb36789
endris <endris@endris-PC>
2016-10-25 23:00:29 +0300

    unique constraint on CrawlingSource.url

--- src/main/java/com/example/homework/CrawlingSource.java
+++ src/main/java/com/example/homework/CrawlingSource.java
@@ -1,5 +1,6 @@
 package com.example.homework;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,6 +12,7 @@ public class CrawlingSource {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private long id;
+    @Column(unique = true)
     private String url;
     private String repositoryKey;
 
