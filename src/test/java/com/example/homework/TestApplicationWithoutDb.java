338d997b50d8cb5f4f581cdd9ddb8a10e41ebb3e
endris <endris@endris-PC>
2016-10-26 20:24:27 +0300

    clean-up

--- src/test/java/com/example/homework/TestApplicationWithoutDb.java
+++ src/test/java/com/example/homework/TestApplicationWithoutDb.java
@@ -2,23 +2,9 @@ package com.example.homework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestDatabase;
 
-@SpringBootApplication(exclude = {
-//    DataSourceAutoConfiguration.class,
-//    DataSourceTransactionManagerAutoConfiguration.class,
-    HibernateJpaAutoConfiguration.class//,
-//    AutoConfigureTestDatabase.class //,
-//    JpaRepositoriesAutoConfiguration.class,
-//    SpringDataWebAutoConfiguration.class,
-//    TransactionAutoConfiguration.class
-})
+@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class})
 public class TestApplicationWithoutDb {
 
     /**
